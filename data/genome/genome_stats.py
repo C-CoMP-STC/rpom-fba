87697b1503c51397d33e8a6936fdefb66f048e35
Adrian Juarez <82752259+acJuarez@users.noreply.github.com>
2025-07-15 14:49:13 -0700

    worked out how to get millimoles per 1 gram DNA of each nucleotide and pyrophosphate and mass of DNA per 1millimole of pyrophosphate

--- data/genome/genome_stats.py
+++ data/genome/genome_stats.py
@@ -20,6 +20,16 @@ def main():
     total_C = 0
     total_A = 0
     total_T = 0
+
+    #the following are molecular weights of each nucelotide (masses in amu). Subrtact a pyrophosphate from each 
+    
+    mw_PPI = 176.9671
+    mw_dATP = 487.15 - mw_PPI
+    mw_dCTP = 463.13 - mw_PPI
+    mw_dGTP = 503.15 - mw_PPI
+    mw_dTTP = 478.14 - mw_PPI
+    avogadro = 6.022e23
+    
     for file in SEQUENCES:
         with open(file, "r") as f:
             # Skip header
@@ -45,9 +55,30 @@ def main():
         total_C += C_nts
         total_A += A_nts
         total_T += T_nts
-    
-    # Print totals
+    #total counts of pyrophosphate should be all counts minus 1 
     total_nts = total_G + total_C + total_A + total_T
+    total_ppi = total_G + total_C + total_A + total_T - 1
+    
+    #mass given in grams for each component
+    mdATP = (total_A * (mw_dATP )) / avogadro
+    mdCTP = (total_C * (mw_dCTP )) / avogadro
+    mdGTP = (total_G * (mw_dGTP )) / avogadro
+    mdTTP = (total_T * (mw_dTTP )) / avogadro
+    mPPI = (total_ppi * (mw_PPI)) / avogadro
+    total_mass = mdATP + mdCTP + mdGTP + mdTTP + mPPI + (mw_PPI/avogadro) #grams
+   
+    #in order to have 1g of DNA we need
+    conversion_factor = 1/total_mass
+    
+    #following should be mmole/1gDNA 
+    mmdATP = (mdATP * conversion_factor) / (mw_dATP*1000)
+    mmdCTP = (mdCTP * conversion_factor) / (mw_dCTP*1000)
+    mmdGTP = (mdGTP * conversion_factor) / (mw_dGTP*1000)
+    mmdTTP = (mdTTP * conversion_factor) / (mw_dTTP*1000)
+    mmPPI = (mPPI * conversion_factor) / (mw_PPI*1000)
+    mDNA = 1/mmPPI #milligrams DNA per 1 mmole pyrophosphate
+
+    # Print totals
     print("=====================================")
     print(f"Total G: {total_G} ({100 * total_G / (total_nts):.2f}%)")
     print(f"Total C: {total_C} ({100 * total_C / (total_nts):.2f}%)")
@@ -56,6 +87,12 @@ def main():
     print(f"Total nucleotides: {total_nts}")
     print("=====================================")
 
+    print(f"mmol per 1 gram dna of dATP {mmdATP} mmol/g")
+    print(f"mmol per 1 gram dna of dCTP  {mmdCTP} mmol/g")
+    print(f"mmol per 1 gram dna of dGTP  {mmdGTP} mmol/g")
+    print(f"mmol per 1 gram dna of dTTP  {mmdTTP} mmol/g")
+    print(f"mmole pyrophosphate per 1 gDNA = {mmPPI}")
+    print(f"grams DNA per mmole PPI = {mDNA/1000}")
 
 if __name__ == "__main__":
     main()
